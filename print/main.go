b44c4e3c88b6bfc8b34d57dd6e95081b6d66d1ed
navneet <navneet.work9@gmail.com>
2024-11-04 03:00:01 +0530

    Taking User Input , Println , Printf

---
+++ print/main.go
@@ -0,0 +1,7 @@
+package main 
+import "fmt"  
+func main() { 
+	age:= 25 
+	name := "navneet"
+	fmt.Println("age :" , age , ", name :" , name ) ; 
+}
---
+++ userinput/main.go
@@ -0,0 +1,24 @@
+package main 
+
+import (
+	"fmt" 
+	// "bufio"
+	// "os"
+)
+
+func main () { 
+    
+	// var name string ; 
+	// fmt.Scan(&name) 
+	// fmt.Println("hello mr. " , name); 
+	var age int ; 
+	fmt.Scan(&age) ; 
+	fmt.Println(age) ;
+	// fmt.Println("Hey! , what's your name") ; 
+    // reader := bufio.NewReader(os.Stdin);
+	// name,_ := reader.ReadString('\n') ; 
+	// fmt.Println(name) ; 
+	var name string ; 
+	fmt.Scan(&name) ; 
+	fmt.Println(name) ; 
+}
