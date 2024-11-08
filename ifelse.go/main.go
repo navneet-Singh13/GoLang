bfb45729911487f3fd88fa77e9e2db7e1393122b
navneet <navneet.work9@gmail.com>
2024-11-09 04:51:05 +0530

    if else conditions in golang

---
+++ ifelse.go/main.go
@@ -0,0 +1,20 @@
+package main 
+
+
+import "fmt" 
+
+func main( ) { 
+	x:=-10 
+
+	if x >= 0 { 
+		 fmt.Println("x is greater than 0 ")
+	} else { 
+		fmt.Println("x is smaller than 0 ")
+	}
+	z := 100 ; 
+	if ( z > 10 ) { 
+		fmt.Println("Z is gr than 10") ; 
+	}
+	
+}
+
